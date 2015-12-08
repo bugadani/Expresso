fe1b87442a269d0eacce32ca84939df111561425
Dániel Buga <daniel@chemium.hu>
2015-12-08 10:39:31 +0100

    Rework array parsing

--- src/Extensions/Core/Parsers/ArrayDefinitionParser.php
+++ src/Extensions/Core/Parsers/ArrayDefinitionParser.php
@@ -2,7 +2,6 @@
 
 namespace Expresso\Extensions\Core\Parsers;
 
-use Expresso\Compiler\Exceptions\ParseException;
 use Expresso\Compiler\Nodes\ArrayDataNode;
 use Expresso\Compiler\Nodes\DataNode;
 use Expresso\Compiler\Nodes\OperatorNode;
@@ -18,12 +17,10 @@ class ArrayDefinitionParser extends Parser
     const TYPE_INDETERMINATE = 0;
     const TYPE_LIST = 1;
     const TYPE_MAP = 2;
-    const TYPE_RANGE = 3;
 
     public function parse(TokenStream $stream, TokenStreamParser $parser)
     {
-        $array = new ArrayDataNode();
-
+        $array    = new ArrayDataNode();
         $listType = self::TYPE_INDETERMINATE;
 
         //Step to the first data token or closing bracket
@@ -33,40 +30,34 @@ class ArrayDefinitionParser extends Parser
             $parser->parse('expression');
             $value = $parser->popOperand();
 
-            //Optional key support
-            if ($this->isRangeOperator($value)) {
-                if (!($listType === self::TYPE_INDETERMINATE)) {
-                    throw new ParseException('Can not mix range definitions with array or map declarations');
-                }
-                $listType = self::TYPE_RANGE;
-                $stream->expectCurrent(Token::PUNCTUATION, ']');
-                $array = $value;
-            } else {
-                if ($stream->current()->test(Token::PUNCTUATION, [':', '=>'])) {
-                    if (!($listType === self::TYPE_INDETERMINATE || $listType === self::TYPE_MAP)) {
-                        throw new ParseException('Can not array and map declarations');
-                    }
-                    $listType = self::TYPE_MAP;
-                    //the previous value was a key
-                    $stream->next();
-                    $parser->parse('expression');
-                    $key   = $value;
-                    $value = $parser->popOperand();
-                } else {
-                    if (!($listType === self::TYPE_INDETERMINATE || $listType === self::TYPE_LIST)) {
-                        throw new ParseException('Can not array and map declarations');
-                    }
-                    $listType = self::TYPE_LIST;
-                    $key   = DataNode::nullNode();
-                }
-
-                $array->add($value, $key);
-                //Elements are comma separated
-                if ($stream->current()->test(Token::PUNCTUATION, ',')) {
-                    $stream->next();
-                } else {
+            if ($listType === self::TYPE_INDETERMINATE) {
+                if ($this->isRangeOperator($value)) {
                     $stream->expectCurrent(Token::PUNCTUATION, ']');
+                    $array = $value;
+                    break;
+                } else if ($stream->current()->test(Token::PUNCTUATION, [':', '=>'])) {
+                    $listType = self::TYPE_MAP;
+                } else {
+                    $key      = DataNode::nullNode();
+                    $listType = self::TYPE_LIST;
                 }
+            }
+
+            if ($listType === self::TYPE_MAP) {
+                $stream->expectCurrent(Token::PUNCTUATION, [':', '=>']);
+                //the previous value was a key
+                $stream->next();
+                $parser->parse('expression');
+                $key   = $value;
+                $value = $parser->popOperand();
+            }
+            $array->add($value, $key);
+
+            //Elements are comma separated
+            if ($stream->current()->test(Token::PUNCTUATION, ',')) {
+                $stream->next();
+            } else {
+                $stream->expectCurrent(Token::PUNCTUATION, ']');
             }
         }
         //push array node to operand stack
