a64eb230afd71219fd4f661546f63d5f7b329654
Dániel Buga <bugadani@gmail.com>
2016-01-12 21:14:02 +0100

    Use String node

--- src/Extensions/Core/Operators/Binary/SimpleAccessOperator.php
+++ src/Extensions/Core/Operators/Binary/SimpleAccessOperator.php
@@ -4,15 +4,15 @@ namespace Expresso\Extensions\Core\Operators\Binary;
 
 use Expresso\Compiler\CompilerConfiguration;
 use Expresso\Compiler\Node;
-use Expresso\Compiler\Nodes\DataNode;
 use Expresso\Compiler\Nodes\IdentifierNode;
+use Expresso\Compiler\Nodes\StringNode;
 
 class SimpleAccessOperator extends ArrayAccessOperator
 {
     public function createNode(CompilerConfiguration $config, Node $left, Node $right)
     {
         if ($right instanceof IdentifierNode) {
-            $right = new DataNode($right->getName());
+            $right = new StringNode($right->getName());
         }
 
         return parent::createNode($config, $left, $right);
