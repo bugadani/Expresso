665ce1bcf0b56a8606d30558af2fa38379631e0d
Dániel Buga <bugadani@gmail.com>
2016-02-04 11:27:29 +0100

    Argument definitions may depend on the previous ones in the same branch

--- test/IntegrationTest.php
+++ test/IntegrationTest.php
@@ -39,7 +39,7 @@ class IntegrationTest extends \PHPUnit_Framework_TestCase
         /** @var \SplFileInfo $file */
         foreach ($iterator as $file) {
             $parsed = $this->parseDescriptor($file);
-            if (is_array($parsed) && $parsed[1] == 'Test nested generators') {
+            if (is_array($parsed)) {
                 yield $file->getPathname() => $parsed;
             }
         }
